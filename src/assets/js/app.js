4e3f6bb6e60059059802e788453ae2cfd869acd5
glorioushedgehog <pdevlin@udallas.edu>
2019-02-09 16:30:37 -0600

    Firebase

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -15,3 +15,43 @@ require('foundation-sites');
 
 
 $(document).foundation();
+
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+// Initialize Firebase
+let config = {
+    apiKey: "AIzaSyCJNFt6hY3AuZoTKPTE-oEhLYbRlu6CHO4",
+    authDomain: "booksharing-40ba7.firebaseapp.com",
+    databaseURL: "https://booksharing-40ba7.firebaseio.com",
+    projectId: "booksharing-40ba7",
+    storageBucket: "booksharing-40ba7.appspot.com",
+    messagingSenderId: "606585176318"
+};
+firebase.initializeApp(config);
+let db = firebase.firestore();
+db.collection("books").get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+    });
+});
+
+function getBookList(){
+    let request = $.ajax({
+        url: "https://booksharing-40ba7.firebaseio.com",
+        method: "GET",
+        dataType: "json"
+    });
+
+    request.done(function( data ) {
+        $( ".data" ).html( data );
+    });
+
+    request.fail(function( jqXHR, textStatus ) {
+        $( ".error" ).html( textStatus );
+    });
+}
+
+//$(document).ready(getBookList);
